fbcb16825b26fc6f5156831cc3dd2dbc9598ac74
Jobin Thavu Varghese <jobintv@gmail.com>
2025-08-11 15:36:53 +0530

    animal magic solution

---
+++ Animal Magic/go.mod
@@ -0,0 +1,3 @@
+module github.com/jobinv12/exercism-golang/animalmagic
+
+go 1.24.2
---
+++ Animal Magic/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// RollADie returns a random int d with 1 <= d <= 20.
+func RollADie() int {
+	min := 1
+	max := 20
+	return rand.Intn(max-min) + min
+}
+
+// GenerateWandEnergy returns a random float64 f with 0.0 <= f < 12.0.
+func GenerateWandEnergy() float64 {
+
+	min := 0.0
+	max := 12.0
+
+	return min + rand.Float64()*(max-min)
+}
+
+// ShuffleAnimals returns a slice with all eight animal strings in random order.
+func ShuffleAnimals() []string {
+	animals := []string{"ant", "beaver", "cat", "dog", "elephant", "fox", "giraffe", "hedgehog"}
+
+	rand.Shuffle(len(animals), func(i, j int) {
+		animals[i], animals[j] = animals[j], animals[i]
+	})
+
+	return animals
+}
+
+func main() {
+
+	fmt.Println(RollADie())
+	fmt.Println(GenerateWandEnergy())
+	fmt.Println(ShuffleAnimals())
+
+}
